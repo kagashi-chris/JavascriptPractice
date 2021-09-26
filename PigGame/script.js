9f01e593d858bdec6b032537965eac4783c4f550
Chris Zhen <chris.zhenartist@gmail.com>
2021-09-26 00:23:18 -0400

    refactored javascript

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,106 +1,104 @@
 'use strict';
 
-let diceRoll = 0;
-let currentScore = 0;
-let playerOneScore = 0;
-let playerTwoScore = 0;
-let currentPlayerTurn = 1;
-const playerScores = document.querySelectorAll('.score');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const player0Score = document.querySelector('#score--0');
+const player1Score = document.getElementById('score--1');
+const player0CurrentScore = document.getElementById('current--0');
+const player1CurrentScore = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
+const buttonNew = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
+let score, currentScore, playerTurn;
+
+function changeActivePlayerCurrentScore(playerTurn, Score) {
+  document.getElementById(`current--${playerTurn}`).textContent = Score;
+}
+
+function changeActivePlayerTotalScore(playerTurn, Score) {
+  document.getElementById(`score--${playerTurn}`).textContent =
+    Score[`${playerTurn}`];
+}
+
+function changePlayerTurn(currentTurn) {
+  if (currentTurn === 0) {
+    playerTurn = 1;
+  } else if (currentTurn === 1) {
+    playerTurn = 0;
+  }
+}
+
+function changeActivePanel(playerTurn) {
+  document
+    .querySelector(`.player--${playerTurn}`)
+    .classList.add('player--active');
+
+  if (playerTurn === 0) {
+    player1.classList.remove('player--active');
+  } else {
+    player0.classList.remove('player--active');
+  }
+}
+
+function declareWinner() {}
 
 function resetGame() {
-  for (let i = 0; i < playerScores.length; i++) {
-    playerScores[i].textContent = 0;
-  }
+  score = [0, 0];
   currentScore = 0;
-  playerOneScore = 0;
-  displayCurrentScore(1);
-  playerTwoScore = 0;
-  displayCurrentScore(2);
-  currentPlayerTurn = 1;
+  playerTurn = 0;
+
   dice.classList.add('hidden');
-  changePanelColor(currentPlayerTurn);
+  player0Score.textContent = score[0];
+  player1Score.textContent = score[1];
+  player0CurrentScore.textContent = 0;
+  player1CurrentScore.textContent = 0;
+  changeActivePanel(playerTurn);
 }
 
-function roll1D6() {
-  diceRoll = Math.trunc(Math.random() * 6 + 1);
-}
+const init = function () {
+  resetGame();
+};
 
-function displayDice() {
+init();
+
+buttonRoll.addEventListener('click', function () {
   if (dice.classList.contains('hidden')) {
     dice.classList.remove('hidden');
   }
-}
 
-function changeDiceImg() {
-  document.getElementById('dice-img').src = `dice-${diceRoll}.png`;
-}
+  //generate random number 1-6
+  let diceRoll = Math.trunc(Math.random() * 6 + 1);
 
-function displayCurrentScore(player) {
-  document.getElementById(`current--${player - 1}`).textContent = currentScore;
-}
+  //display the new number
+  dice.src = `dice-${diceRoll}.png`;
 
-function displayTotalScore(currentPlayerTurn, score) {
-  document.getElementById(`score--${currentPlayerTurn - 1}`).textContent =
-    score;
-}
-
-function changePanelColor(currentPlayerTurn) {
-  if (currentPlayerTurn === 1) {
-    document.querySelector('.player--0').classList.add('player--active');
-    document.querySelector('.player--1').classList.remove('player--active');
+  //see if 1 is rolled
+  if (diceRoll === 1) {
+    currentScore = 0;
+    changeActivePlayerCurrentScore(playerTurn, currentScore);
+    changePlayerTurn(playerTurn);
+    changeActivePanel(playerTurn);
   } else {
-    document.querySelector('.player--1').classList.add('player--active');
-    document.querySelector('.player--0').classList.remove('player--active');
+    currentScore += diceRoll;
+    changeActivePlayerCurrentScore(playerTurn, currentScore);
   }
-}
-
-function switchTurn() {
-  if (currentPlayerTurn === 1) {
-    currentPlayerTurn = 2;
-  } else {
-    currentPlayerTurn = 1;
-  }
-}
-
-function checkDiceRoll() {
-  if (diceRoll != 1) {
-  }
-}
-
-resetGame();
-
-document.querySelector('.btn--new').addEventListener('click', resetGame);
-
-document.querySelector('.btn--hold').addEventListener('click', function hold() {
-  let score = 0;
-  if (currentPlayerTurn === 1) {
-    playerOneScore += currentScore;
-    score = playerOneScore;
-  } else {
-    playerTwoScore += currentScore;
-    score = playerTwoScore;
-  }
-  displayTotalScore(currentPlayerTurn, score);
-  currentScore = 0;
-  displayCurrentScore(currentPlayerTurn);
-  switchTurn();
-  changePanelColor(currentPlayerTurn);
 });
 
-document
-  .querySelector('.btn--roll')
-  .addEventListener('click', function rollDice() {
-    roll1D6();
-    displayDice();
-    changeDiceImg();
-    if (diceRoll != 1) {
-      currentScore += diceRoll;
-      displayCurrentScore(currentPlayerTurn);
-    } else if (diceRoll === 1) {
-      currentScore = 0;
-      displayCurrentScore(currentPlayerTurn);
-      switchTurn();
-      changePanelColor(currentPlayerTurn);
-    }
-  });
+buttonHold.addEventListener('click', function () {
+  //add current score to active player total
+  score[`${playerTurn}`] += currentScore;
+  changeActivePlayerTotalScore(playerTurn, score);
+
+  //reset current score and display it
+  currentScore = 0;
+  changeActivePlayerCurrentScore(playerTurn, currentScore);
+
+  //change player turn
+  changePlayerTurn(playerTurn);
+  changeActivePanel(playerTurn);
+});
+
+buttonNew.addEventListener('click', function () {
+  resetGame();
+});
