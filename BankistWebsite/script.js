106411b614b68cdb1b1074385c3e3484f564c4d8
Chris Zhen <chris.zhenartist@gmail.com>
2021-10-04 13:40:55 -0400

    faded menu animations

--- BankistWebsite/script.js
+++ BankistWebsite/script.js
@@ -7,6 +7,16 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const logo = document.querySelector('.nav__logo');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -34,11 +44,7 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 ///////////////////////////////////////
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-
 document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
 
 const message = document.createElement('div');
 message.classList.add('cookie-message');
@@ -61,11 +67,6 @@ message.style.height =
 
 //button scrolling
 
-const logo = document.querySelector('.nav__logo');
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
@@ -80,11 +81,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 //Tabbed component
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabContainer = document.querySelector('.operations__tab-container');
-const tabContent = document.querySelectorAll('.operations__content');
-
 tabContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -103,4 +99,35 @@ tabContainer.addEventListener('click', function (e) {
   document
     .querySelector(`.operations__content--${ele}`)
     .classList.add('operations__content--active');
+});
+
+//Menu fade animation
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 0.5;
+        logo.style.opacity = 0.5;
+      }
+    });
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 1;
+        logo.style.opacity = 1;
+      }
+    });
+  }
 });
